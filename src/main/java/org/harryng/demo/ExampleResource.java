eff5ffdd82147e25e9b5f167d002f5f3a4d6ccf2
harrynguyen0312 <Hiepxie1987>
2022-10-18 00:12:47 +0700

    Initial commit

---
+++ src/main/java/org/harryng/demo/ExampleResource.java
@@ -0,0 +1,20 @@
+package org.harryng.demo;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class ExampleResource {
+
+    @Inject
+    protected GreetingConfig greetingConfig;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Reactive " + greetingConfig.message();
+    }
+}
